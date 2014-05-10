ea07376df6ef1d27256c012259670ecb3e7568fa
Qemist0365 <raze0365@live.dk>
2014-05-10 21:09:39 +0200

    Added plugin autodata
    Can be configured via the sh_config.lua file
    Includes example for citizen and CP
    
    Also adds a command /resetdata to reset a characters combine data to the
    default

---
+++ autodata/sh_config.lua
@@ -0,0 +1,20 @@
+nut.config.autoData = {
+	[FACTION_CP] = [[
+	----------------------
+	Unit's Name: %name%
+	----------------------
+	Digits: %digits%
+	----------------------
+	]],
+	[FACTION_CITIZEN] = [[
+	----------------------
+	Citizens' Name: %name%
+	----------------------
+	Citizen ID: %digits%
+	----------------------
+	Points: 0
+	----------------------
+	Loyalty Points: 0
+	----------------------
+	]]
+}
